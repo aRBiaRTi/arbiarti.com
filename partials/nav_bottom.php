a67699e1ad67286e81c05ae2e5d15e72b9ece4e1
Roberto A. Inzunza A. <rbrt.nznz.vls@gmail.com>
2025-01-21 03:32:58 -0700

    Reacomodo de navegador dos

--- partials/nav_bottom.php
+++ partials/nav_bottom.php
@@ -1,7 +1,7 @@
 	<nav class="bottom">
 		<!-- <div class="separador"></div> -->
-		<!-- <button onClick="window.location.href='index.php'">Inicio</button> -->
+		<button onClick="window.location.href='index.php'">Inicio</button>
 		<!-- <button onClick="window.location.href='herramientas.php'">Herramientas</button> -->
-		<button onClick="window.location.href='sobremi.php'">Sobre mí</button>
+		<!--<button onClick="window.location.href='sobremi.php'">Sobre mí</button>-->
 		<button onClick="window.location.href='portafolio.php'">Portafolio</button>
 	</nav>
