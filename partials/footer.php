28f2ca52b74f41642b5e0043de504606d01fd743
Roberto A. Inzunza A. <rbrt.nznz.vls@gmail.com>
2025-01-21 03:30:18 -0700

    Reacomodo de enlaces en el pie de pagina

--- partials/footer.php
+++ partials/footer.php
@@ -3,21 +3,26 @@
 		<div>
 			<h3>aRBiaRTi</h3>
 			<ul>
-				<li><a href="/index.php">Inicio</a></li>
 				<li><a href="/portafolio.php">Portafolio</a></li>
 				<li><a href="/sobremi.php">Sobre mí</a></li>
 			</ul>
+		</div>
+		<div>
 			<h3>Redes de Trabajo</h3>
 			<ul>
 				<li><a href="https://www.linkedin.com/in/rbrtnznz/">LinkedIn</a></li>
 				<li><a href="https://github.com/aRBiaRTi">Github</a></li>
 				<li><a href="https://www.youtube.com/@rbrtnznz">Youtube</a></li>
 			</ul>
+		</div>
+		<div>
 			<h3>Redes Sociales</h3>
 			<ul>
 				<li><a href="https://www.facebook.com/RbrtNznz">Facebook</a></li>
 				<li><a href="https://www.instagram.com/roberto.inzunza.vls/">Instagram</a></li>
 			</ul>
+		</div>
+		<div>
 			<h3>Redes de Videojuegos</h3>
 			<ul>
 				<li><a href="https://steamcommunity.com/id/arbiarti/">Steam</a></li>
@@ -25,7 +30,4 @@
 				<li><a href="https://www.twitch.tv/arbiarti">Twitch</a></li>
 			</ul>
 		</div>
-		<div></div>
-		<div></div>
-		<div></div>
 	</footer>
