5a7e2d75dc94587afc41a9fbb9fd63a9d77c1573
Roberto A. Inzunza A. <rbrt.nznz.vls@gmail.com>
2025-01-21 16:02:44 -0700

    Formato de pagina

--- index.php
+++ index.php
@@ -17,31 +17,63 @@
 		<aside>
 			<div class="sobre_mi">
 				<h3>SOBRE MI</h3>
-				<p>Me apasiona el manejo de sistemas operativos, entre ellos Windows, Windows Server, Ubuntu, Ubuntu Server y mi favorito Arch Linux.</p>
+				<p>Me apasiona todo lo que tiene que ver en el mundo de la informática, lo que más me gusta es el manejo de sistemas operativos, entre ellos Windows, Windows Server, Ubuntu, Ubuntu Server y mi favorito Arch Linux.</p>
 				<p>Me gusta crear servidores de videojuegos, usando esta plataforma para alojarlos y jugar con mis amigos, entre ellos Minecraft, Killing Floor y Counter Strike 1.6</p>
 			</div>
 		</aside>
 		<section>
+			<div style="display:grid; justify-items: start; gap: 10px;">
+				<h2>SERVIDOR DE MINECRAFT</h2>
+				<div style="padding: 10px; display:grid; grid-auto-flow:column; gap:10px;">
+					<img src="public/imgs/minecraft_logo.png" width="100">
+					<div style="display:grid; grid-auto-flow:row; align-content:start; gap: 5px;">
+						<p>IP: arbiarti.com</p>
+						<p>Versión: Java 1.21</p>
+						<p>Modo de juego: Supervivencia</p>
+					</div>
+				</div>
+			</div>
 			<div class="exp_laboral">
-				<h3>EXPERIENCIA LABORAL</h3>
+				<h2>EXPERIENCIA LABORAL</h2>
 				<h4>Minisuper Chulavista</h4>
-				<p>Mis labores eran administrativas y manejo de personal especialmente, pero más enfocado en mi area realice armado de computadoras, instalación y mantenimiento de cámaras de seguridad, llegué a realizar scripts para extraer información del sistema ya que el que se usa es muy obsoleto</p>
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1811.5583355274043!2d-107.41512916982174!3d24.757188583704618!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86bcd1d7fece0401%3A0x62ec19b32f7b0d96!2sMini%20super%20Chulavista!5e0!3m2!1ses-419!2smx!4v1737460373127!5m2!1ses-419!2smx" width="200" height="150" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				<p>Mis principales labores eran administrativas y manejo de personal, pero más enfocado en mi area realice armado de computadoras, instalación y mantenimiento de cámaras de seguridad, llegué a realizar scripts para extraer información del sistema ya que el que se usa es muy obsoleto</p>
+				<div>
+					<img src="public/imgs/minisuper_chulavista_fachada.png" width="500">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1811.5583355274043!2d-107.41512916982174!3d24.757188583704618!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86bcd1d7fece0401%3A0x62ec19b32f7b0d96!2sMini%20super%20Chulavista!5e0!3m2!1ses-419!2smx!4v1737460373127!5m2!1ses-419!2smx" width="230" height="230.467" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				</div>
 				<h4>Power Hosting & Cloud</h4>
-				<p>Trabajando como practicante, llegué a aplicar lo aprendido en la Universidad Autónoma de Sinaloa para hacer auditorias, entre las cosas que aprendí dentro de la empresa es el manejo de VMWare para otorgar espacios para algunas sucursales de otras empresas</p>
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1810.976645201979!2d-107.40647747353657!3d24.79705291821075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86bcd0bc3355a5d3%3A0xbb82ee6a9d5d5934!2sPower%20Cloud%20and%20Hosting!5e0!3m2!1ses-419!2smx!4v1737460467435!5m2!1ses-419!2smx" width="200" height="150" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				<p>Trabajando como practicante, llegué a aplicar lo aprendido en la Universidad Autónoma de Sinaloa para hacer auditorias, pude ver como estaban aplicados las normas correspondientes a la norma ISO 27001, entre las cosas que aprendí dentro de la empresa es el manejo de VMWare para otorgar espacios para algunas sucursales de otras empresas</p>
+				<div>
+					<img src="public/imgs/powerhost_monitores.png" width="400">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1810.976645201979!2d-107.40647747353657!3d24.79705291821075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86bcd0bc3355a5d3%3A0xbb82ee6a9d5d5934!2sPower%20Cloud%20and%20Hosting!5e0!3m2!1ses-419!2smx!4v1737460467435!5m2!1ses-419!2smx" width="266.55" height="267" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				</div>
 				<h4>Minisuper Imperial</h4>
 				<p>Durante todo el tiempo que estuve trabajando para esa sucursal estuve de cajero cubriendo descansos, pero en muchas ocasiones tuve que realizar mantenimiento y reparación de computadoras</p>
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3170.605419121378!2d-107.41493089657025!3d24.757063977879504!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86bcd1517091aecb%3A0xcdaf23c0d12aebe!2sMinisuper%20Imperial!5e0!3m2!1ses-419!2smx!4v1737460317392!5m2!1ses-419!2smx" width="200" height="150" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				<div>
+					<img src="public/imgs/minisuper_imperial_fachada.png" width="500">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3170.605419121378!2d-107.41493089657025!3d24.757063977879504!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86bcd1517091aecb%3A0xcdaf23c0d12aebe!2sMinisuper%20Imperial!5e0!3m2!1ses-419!2smx!4v1737460317392!5m2!1ses-419!2smx" width="239" height="238.667" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				</div>
 			</div>
 			<div class="formacion">
-				<h3>FORMACIÓN</h3>
+				<h2>FORMACIÓN</h2>
 				<h4>Universidad Autónoma de Sinaloa</h4>
 				<p>Aprendí analisis de datos, protección de la información, diversas formas de ataques informáticos, manejo de sistemas operativos, entre otras habilidades.</p>
+				<div>
+					<img src="public/imgs/uadeo_fachada.png" width="500">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7242.982327688375!2d-107.43776024488739!3d24.81287146909646!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86bcda7e6201ae63%3A0xd3eeeda6abe89a3c!2sUniversidad%20Aut%C3%B3noma%20de%20Occidente!5e0!3m2!1ses-419!2smx!4v1737496270014!5m2!1ses-419!2smx" width="273" height="272.983" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				</div>
 				<h4>Instituto Tecnológico de Culiacán</h4>
 				<p>Aprendí los procedimientos que se deben realizar para la creación de un software de información, manejo de base de datos y programación.</p>
+				<div>
+					<img src="public/imgs/tec_fachada.png" width="500">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3622.1899074410167!2d-107.39925762564619!3d24.788949877976005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86bcd0c61991131b%3A0x918d0686e996a928!2sTecnol%C3%B3gico%20Nacional%20de%20M%C3%A9xico%20campus%20Culiac%C3%A1n!5e0!3m2!1ses-419!2smx!4v1737496301466!5m2!1ses-419!2smx" width="271" height="271.35" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				</div>
 				<h4>Colegio Nacional de Educación Profesional Técnica</h4>
 				<p>Aprendí las bases de la informática, mantenimiento y reparación de equipos de computo, manejo de redes, programación avanzada, manejo de bases de datos.</p>
+				<div>
+					<img src="public/imgs/conalep_fachada.png" width="500">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3622.689740886362!2d-107.4571474282197!3d24.77182472933686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86bcd12c1bdd4755%3A0xdc5454f8f348723d!2sConalep%20Juan%20de%20Dios%20B%C3%A1tiz%20-%20Culiac%C3%A1n%20I!5e0!3m2!1ses-419!2smx!4v1737496365720!5m2!1ses-419!2smx" width="189" height="188.667" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				</div>
 			</div>
 		</section>
 	</main>
