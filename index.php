b4ddac38c55686552ede661793fd91f5ac3b6058
Roberto A. Inzunza A. <rbrt.nznz.vls@gmail.com>
2025-01-21 05:02:50 -0700

    Adición de curriculum a la pagina inicio

--- index.php
+++ index.php
@@ -14,26 +14,35 @@
 	?>
 
 	<main>
-		<aside></aside>
-		<section class="contenido">
-			<nav class="contenido"></nav>
-			<div class="post">
-				<img src="">
-				<div class="header">
-					<h2>¡Hola Mundo!</h2>
-					<div class="datos">
-						Viernes 22 de Septiembre de 2023, por Roberto A. Inzunza A; desde aRBiaRTi.com.
-					</div>
-				</div>
-				<p class="descripcion">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." [Mostrar más...]</p>
+		<aside>
+			<div class="sobre_mi">
+				<h3>SOBRE MI</h3>
+				<p>Me apasiona el manejo de sistemas operativos, entre ellos Windows, Windows Server, Ubuntu, Ubuntu Server y mi favorito Arch Linux.</p>
+				<p>Me gusta crear servidores de videojuegos, usando esta plataforma para alojarlos y jugar con mis amigos, entre ellos Minecraft, Killing Floor y Counter Strike 1.6</p>
 			</div>
-			<div class="post"></div>
-			<div class="post"></div>
-			<div class="post"></div>
-			<div class="post"></div>
-			<div class="post"></div>
-			<div class="post"></div>
-			<footer class="contenido"></footer>
+		</aside>
+		<section>
+			<div class="exp_laboral">
+				<h3>EXPERIENCIA LABORAL</h3>
+				<h4>Minisuper Chulavista</h4>
+				<p>Mis labores eran administrativas y manejo de personal especialmente, pero más enfocado en mi area realice armado de computadoras, instalación y mantenimiento de cámaras de seguridad, llegué a realizar scripts para extraer información del sistema ya que el que se usa es muy obsoleto</p>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1811.5583355274043!2d-107.41512916982174!3d24.757188583704618!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86bcd1d7fece0401%3A0x62ec19b32f7b0d96!2sMini%20super%20Chulavista!5e0!3m2!1ses-419!2smx!4v1737460373127!5m2!1ses-419!2smx" width="200" height="150" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				<h4>Power Hosting & Cloud</h4>
+				<p>Trabajando como practicante, llegué a aplicar lo aprendido en la Universidad Autónoma de Sinaloa para hacer auditorias, entre las cosas que aprendí dentro de la empresa es el manejo de VMWare para otorgar espacios para algunas sucursales de otras empresas</p>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1810.976645201979!2d-107.40647747353657!3d24.79705291821075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86bcd0bc3355a5d3%3A0xbb82ee6a9d5d5934!2sPower%20Cloud%20and%20Hosting!5e0!3m2!1ses-419!2smx!4v1737460467435!5m2!1ses-419!2smx" width="200" height="150" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				<h4>Minisuper Imperial</h4>
+				<p>Durante todo el tiempo que estuve trabajando para esa sucursal estuve de cajero cubriendo descansos, pero en muchas ocasiones tuve que realizar mantenimiento y reparación de computadoras</p>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3170.605419121378!2d-107.41493089657025!3d24.757063977879504!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86bcd1517091aecb%3A0xcdaf23c0d12aebe!2sMinisuper%20Imperial!5e0!3m2!1ses-419!2smx!4v1737460317392!5m2!1ses-419!2smx" width="200" height="150" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+			</div>
+			<div class="formacion">
+				<h3>FORMACIÓN</h3>
+				<h4>Universidad Autónoma de Sinaloa</h4>
+				<p>Aprendí analisis de datos, protección de la información, diversas formas de ataques informáticos, manejo de sistemas operativos, entre otras habilidades.</p>
+				<h4>Instituto Tecnológico de Culiacán</h4>
+				<p>Aprendí los procedimientos que se deben realizar para la creación de un software de información, manejo de base de datos y programación.</p>
+				<h4>Colegio Nacional de Educación Profesional Técnica</h4>
+				<p>Aprendí las bases de la informática, mantenimiento y reparación de equipos de computo, manejo de redes, programación avanzada, manejo de bases de datos.</p>
+			</div>
 		</section>
 	</main>
 
